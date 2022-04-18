76045fddc80ff257162ad896cd4eedb9083a3e65
Godefroi Roussel <godefroi.roussel@pingoucv.com>
2022-04-18 11:16:37 +0200

    refactor: separate usecase from the test file

--- src/core/Game/__tests__/addShot.test.ts
+++ src/core/Game/__tests__/addShot.test.ts
@@ -4,26 +4,7 @@ import { Coordinate, CoordinateShot, TYPE_COORDINATE } from '../domain/entity/co
 import { Game } from '../domain/entity/game';
 import { Player } from '../domain/entity/player';
 import { Ship, TYPE_SHIP } from '../domain/entity/ship';
-import { IGameRepository } from '../domain/repository';
-
-class ShotUsecase {
-  constructor(private gameRepository: IGameRepository) {}
-
-  async execute(idGame: string, idPlayer: string, coordinateToShoot: Coordinate) {
-    const game = await this.gameRepository.getById(idGame);
-    if (!game) {
-      throw new Error('Game not found');
-    }
-
-    const currentPlayerToShoot = game.player1?.id === idPlayer ? game.player1 : game.player2;
-    const otherPlayer = game.player1 === currentPlayerToShoot ? game.player2 : game.player1;
-    if (!currentPlayerToShoot || !otherPlayer) {
-      throw new Error('A game must have players to be played');
-    }
-    currentPlayerToShoot.shot(coordinateToShoot, otherPlayer);
-    await this.gameRepository.update(game);
-  }
-}
+import { ShotUsecase } from '../domain/use-cases/shotUsecase';
 
 let game: Game;
 let player: Player;
