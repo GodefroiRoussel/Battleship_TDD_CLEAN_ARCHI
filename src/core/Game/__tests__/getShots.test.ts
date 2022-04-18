82e360370696c34cbcd7cd6a892fad84b60b24ce
Godefroi Roussel <godefroi.roussel@pingoucv.com>
2022-04-18 11:30:31 +0200

    feat: get shots of a player

---
+++ src/core/Game/__tests__/getShots.test.ts
@@ -0,0 +1,24 @@
+import { InMemoryGameRepository } from '../adapters/in-memory/InMemoryGameRepository';
+import { StubGameBuilder } from '../domain/entity/builder/gameBuilder';
+import { CoordinateShot, TYPE_COORDINATE } from '../domain/entity/coordinate';
+import { Player } from '../domain/entity/player';
+import { GetShotsUsecase } from '../domain/use-cases/getShotsUsecase';
+
+describe('Scenario: During the game, when I am a player, I want to see my previously shots', () => {
+  it('should return all my shots information', async () => {
+    // Arrange
+    const waterShot = new CoordinateShot(5, 5, TYPE_COORDINATE.WATER);
+    const touchedShot = new CoordinateShot(5, 6, TYPE_COORDINATE.TOUCHED);
+    const expectedShots = [waterShot, touchedShot];
+    const player = new Player('id1', 'name1', [], [waterShot, touchedShot]);
+    const game = new StubGameBuilder().withPlayer1(player).build();
+    const inMemoryGameRepository = new InMemoryGameRepository([game]);
+    const getShotsUsecase = new GetShotsUsecase(inMemoryGameRepository);
+
+    // Act
+    const shots = await getShotsUsecase.execute(game.id, player.id);
+
+    // Assert
+    expect(expectedShots).toEqual(shots);
+  });
+});
---
+++ src/core/Game/domain/use-cases/getShotsUsecase.ts
@@ -0,0 +1,19 @@
+import { CoordinateShot } from '../entity/coordinate';
+import { IGameRepository } from '../repository';
+
+export class GetShotsUsecase {
+  constructor(private gameRepository: IGameRepository) {}
+
+  async execute(idGame: string, idPlayer: string): Promise<CoordinateShot[]> {
+    const game = await this.gameRepository.getById(idGame);
+    if (!game) {
+      throw new Error('Game not found');
+    }
+
+    const player = game.player1?.id === idPlayer ? game.player1 : game.player2;
+    if (!player) {
+      throw new Error('A game must have players to be played');
+    }
+    return Promise.resolve(player.listCoordinatesShot);
+  }
+}
